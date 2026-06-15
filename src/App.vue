f3d07a861d929a514c70fe522235186c937c6373
Nodirbek <ndilmurodov19@gmail.com>
2026-06-15 16:25:03 +0500

    carousel

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,38 @@
 <template>
  <router-view></router-view>
+ <div class="carousel-container">
+    <ejs-carousel>
+        <e-carousel-items>
+            <e-carousel-item template = "<figure>
+                <img src='' alt='cardinal'/>
+            </figure>"></e-carousel-item>
+            <e-carousel-item  template = "<figure>
+                <img src='' alt='cardinal'/>
+            </figure>"></e-carousel-item>
+            <e-carousel-item  template = "<figure>
+                <img src='' alt='cardinal'/>
+            </figure>"></e-carousel-item>
+            <e-carousel-item  template = "<figure>
+                <img src='' alt='cardinal'/>
+            </figure>"></e-carousel-item>
+            <e-carousel-item  template = "<figure>
+                <img src='' alt='cardinal'/>
+            </figure>"></e-carousel-item>
+        </e-carousel-items>
+    </ejs-carousel>
+ </div>
 </template>
+<script setup lang ="ts">
+import {
+    CarouselComponent,
+    CarouselItemDirective,
+    CarouselItemDirective
+} from "@syncfusion/ej2-vue-navigations";
+export default {
+    components: {
+        'ejs-carousel' : CarouselComponent,
+        'e-carousel-item' : CarouselItemDirective,
+        'e-carousel-items' : CarouselItemDirective
+        }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.ts'
+import{registerLicense} from '@syncfusion/ej2-base'
+registerLicense("ORg4AjUWIQA/Gnt2VFhiQ1hPcEBAX3xLf1F1VWJbdVpxf1ZEcDwsT3RfQF5jT3xRdkJmUX1YdHJTTg==");
 
 createApp(App).use(router).mount('#app')
