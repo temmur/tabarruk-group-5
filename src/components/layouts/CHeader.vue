d544b4fa76f67cdec4e80789ee80dcf2f9866469
Abdulbosit <valiyevabdulbosit9@gmail.com>
2026-06-15 15:38:59 +0500

    creating header

--- src/components/layouts/CHeader.vue
+++ src/components/layouts/CHeader.vue
@@ -1,3 +1,15 @@
 <template>
-    CHeader
+    <div class="c-header">
+        <div class="c-header__logo">
+        <img src="./images/" alt="Logo" />
+        </div>
+        <nav class="c-header__nav">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        </nav>
+    </div>          
 </template>
