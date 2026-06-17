d5fd086f4de228907f4c8136c25aa70ce0de34f7
Nodirbek <ndilmurodov19@gmail.com>
2026-06-17 16:30:04 +0500

    feat: CBannerCard

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,4 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.ts'
-import{registerLicense} from '@syncfusion/ej2-base'
-registerLicense("ORg4AjUWIQA/Gnt2VFhiQ1hPcEBAX3xLf1F1VWJbdVpxf1ZEcDwsT3RfQF5jT3xRdkJmUX1YdHJTTg==");
-
 createApp(App).use(router).mount('#app')
