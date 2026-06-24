b8230d78bbef8403a551f97756e919e1971f579d
Mirfayz <shnasriddinov77@gmail.com>
2026-06-24 17:46:24 +0500

    created CABout

--- src/views/PHome.vue
+++ src/views/PHome.vue
@@ -1,6 +1,10 @@
 <template>
-      <h1 class="text-red-500">Home page</h1>
+      <div>
+            <CAbout/>
+      </div>
 </template>
 <script setup lang="ts">
-import {ref} from 'vue'
+import CAbout from '@/components/sections/CAbout.vue';
+import CInput from '@/components/Form/CInput.vue';
+
 </script>
